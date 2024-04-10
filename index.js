ab43011909e874f580b024cd30f77632a30b7603
Giritharan <70332966+giri14mct@users.noreply.github.com>
2024-04-10 13:47:09 +0530

    Feat: Initial commit for the repo with some basic setup

---
+++ index.js
@@ -0,0 +1,54 @@
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (req, h) => {
+      return "Hello from HapiJS!";
+    },
+  });
+
+  server.route({
+    method: "GET",
+    path: "/movies",
+    handler: (req, h) => {
+      return "List Of All movies";
+    },
+  });
+
+  server.route({
+    method: "POST",
+    path: "/movies",
+    handler: (req, h) => {
+      return `${req.payload.name} Movie Created`;
+    },
+  });
+
+  server.route({
+    method: "PUT",
+    path: "/movies/{id}",
+    handler: (req, h) => {
+      console.log(req.params.id, "h");
+      return `${req.params.id} Movie Updated`;
+    },
+  });
+
+  server.route({
+    method: "DELETE",
+    path: "/movies/{id}",
+    handler: (req, h) => {
+      return `${req.params.id} Movie Deleted`;
+    },
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+
+init();
